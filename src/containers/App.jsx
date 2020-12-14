0e8324067e12bc284c774749b96c94c9629216ea
afelipelds <afelipelds.ez@gmail.com>
2020-12-14 17:13:04 -0500

    App component: main component | Media styles

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
 import '../assets/styles/App.scss'
+import Search from '../components/Search'
+import Categories from '../components/Categories'
+import Carousel from '../components/Carousel'
+import CarouselItem from '../components/CarouselItem'
 
-const App = () => (
-    <>
-        <Header />
-        {/* <Register /> */}
-        <Home />
-        {/* <Podcast /> */}
-        {/* <Login /> */}
-        <Footer />
-    </>
-)
+
+const App = () => {
+    const API = 'http://localhost:3000/initalState'
+    const [videos, setVideos] = useState({
+        mylist: [],
+        trends: [],
+        originals: []
+    })
+    useEffect( () => {
+        fetch(API)
+            .then( response => response.json() )
+            .then( data => setVideos(data) ) /** Se asigna nuevos valores a videos */
+    }, [] );
+
+    return (
+        <>
+            <Header />
+            {/* <Register /> */}
+            <Search />
+            {videos.mylist.length > 0 && (
+                <Categories title={"Mi lista"}>
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            )}
+
+            <Categories title={"Originals"}>
+                <Carousel>
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            
+            {videos.trends !== undefined && (
+                <Categories title={"Trend"}>
+                    <Carousel>
+                        {videos.trends.map( item => {
+                            <CarouselItem key={item.id} {...item} />
+                        })}
+
+                    </Carousel>
+                </Categories>
+            )}
+            {/* <Podcast /> */}
+            {/* <Login /> */}
+            <Footer />
+        </>
+    ) 
+}
 
 export default App
