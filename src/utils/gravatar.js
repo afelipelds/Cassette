dbe02c123d0c679a5592ea8553ced6ab09ac08c5
afelipelds <afelipelds.ez@gmail.com>
2021-01-11 23:56:53 -0500

    Implementación de gravatar forma de llamar la imagen de Gravatar para iniciar sesión.

--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -3,10 +3,31 @@ import md5 from 'md5';
 const gravatar = (email) => {
   const base = 'https://gravatar.com/avatar/';
   const formattedEmail = email.trim().toLowerCase();
-  // como no podemos enviar símbolos como el '@' debemos convertir el email a formato binario
-  const hash = md5(formattedEmail, { encoding: 'binary' });
-  console.log(hash);
+  const hash = md5(formattedEmail, { encoding: 'binary' }); // como no podemos enviar símbolos como el '@' debemos convertir el email a formato binario
   return `${base}${hash}`;
 };
 
+/*
+ * Recursividad : Trampolines: realizan un mejor performance de procesamiento
+ * para evitar al Call tail y que éste se bloquee
+*/
+/* const suma = (number, sum = 0) => {
+  console.log('number is: ', number, 'sum is: ', sum);
+  return number === 0 ? sum : () => suma(number - 1, sum + number);
+};
+
+const trampoline = func => (...args) => {
+  let result = func(...args);
+  console.log('result', typeof result, result);
+  while (typeof result === 'function') {
+    result = result();
+  };
+
+  return result;
+};
+
+const tramp = trampoline(suma);
+const res = tramp(10000);
+console.log('res ->', res);
+ */
 export default gravatar;
