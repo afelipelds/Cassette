3059504c30fb0e9c8a612297e176106d84f3df7f
afelipelds <afelipelds.ez@gmail.com>
2021-01-11 23:54:28 -0500

    Actualización de constantes: nombres de actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,21 @@
+import { SET_FAVORITE, DELETE_FAVORITE, LOGIN_REQUEST, LOGOUT_REQUEST } from '../utils/constants';
+
 export const setFavorite = (payload) => ({
-  type: 'SET_FAVORITE',
+  type: SET_FAVORITE,
   payload,
 });
 
 export const deleteFavorite = (payload) => ({
-  type: 'DELETE_FAVORITE',
+  type: DELETE_FAVORITE,
   payload,
 });
 
 export const loginRequest = (payload) => ({
-  type: 'LOGIN_REQUEST',
+  type: LOGIN_REQUEST,
   payload,
-}) ;
+});
+
+export const logoutRequest = (payload) => ({
+  type: LOGOUT_REQUEST,
+  payload,
+});
